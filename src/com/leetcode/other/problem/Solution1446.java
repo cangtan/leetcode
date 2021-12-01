88d7efe7765a547bb748f13de940425f74059565
xuejiahao <jiahao.xue@nriat.com>
2021-12-01 15:58:28 +0800

    feat: 每日一题：连续字符

---
+++ src/com/leetcode/other/problem/Solution1446.java
@@ -0,0 +1,31 @@
+package com.leetcode.other.problem;
+
+/**
+ * 连续字符
+ *
+ * @author 薛佳豪
+ * @version V1.0
+ * @date 2021-12-01 14:34:00
+ */
+public class Solution1446 {
+    public int maxPower(String s) {
+        int result = 1;
+        int temp = 1;
+        char[] chars = s.toCharArray();
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                temp++;
+                result = Math.max(result, temp);
+            } else {
+                temp = 1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution1446 solution = new Solution1446();
+        String s = "hooraaaaaaaaaaay";
+        System.out.println(solution.maxPower(s));
+    }
+}
