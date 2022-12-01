bdff25e788a3fb093c2d3626b00217b2db124f96
xuejiahao <jiahao.xue@nriat.com>
2022-12-01 14:09:47 +0800

    feat: 1779 找到最近的有相同X或Y坐标的点

---
+++ src/com/leetcode/other/problem/Solution1779.java
@@ -0,0 +1,34 @@
+package com.leetcode.other.problem;
+
+/**
+ * 找到最近的有相同X或Y坐标的点
+ *
+ * @author 薛佳豪
+ * @version V1.0
+ * @since 2022-12-01 09:12:00
+ */
+public class Solution1779 {
+    public int nearestValidPoint(int x, int y, int[][] points) {
+        int minIndex = -1;
+        int minValue = Integer.MAX_VALUE;
+        for (int i = 0; i < points.length; i++) {
+            int[] point = points[i];
+            if (point[0] == x || point[1] == y) {
+                int dist = Math.abs(x - point[0]) + Math.abs(y - point[1]);
+                if (minIndex == -1 || dist < minValue) {
+                    minIndex = i;
+                    minValue = dist;
+                }
+            }
+        }
+        return minIndex;
+    }
+
+    public static void main(String[] args) {
+        Solution1779 solution = new Solution1779();
+        int x = 3;
+        int y = 4;
+        int[][] points = {{1, 2}, {3, 1}, {2, 4}, {2, 3}, {4, 4}};
+        System.out.println(solution.nearestValidPoint(x, y, points));
+    }
+}
