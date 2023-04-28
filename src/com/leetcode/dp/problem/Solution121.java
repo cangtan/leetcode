794cb4804467623b12f19faf3e0ea4a1985a9057
xuejiahao <jiahao.xue@nriat.com>
2023-04-28 17:05:43 +0800

    feat: 买卖股票的最佳时机,无滚动,dp数组解法

--- src/com/leetcode/dp/problem/Solution121.java
+++ src/com/leetcode/dp/problem/Solution121.java
@@ -21,9 +21,26 @@ public class Solution121 {
         return result;
     }
 
+    public int maxProfit2(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[prices.length][2];
+        // 0 表示买入
+        dp[0][0] = prices[0];
+        // 1 表示卖出
+        dp[0][1] = 0;
+        for (int i = 1; i < prices.length; i++) {
+            // 我们希望买入的价格越低越好
+            dp[i][0] = Math.min(dp[i - 1][0], prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], prices[i] - dp[i - 1][0]);
+        }
+        return dp[prices.length - 1][1];
+    }
+
     public static void main(String[] args) {
         Solution121 solution = new Solution121();
         int[] prices = {7, 1, 5, 3, 6, 4};
-        System.out.println(solution.maxProfit(prices));
+        System.out.println(solution.maxProfit2(prices));
     }
 }
