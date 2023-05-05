00af2ae756a4dc967ed228c1599a52cad6a80b5f
xuejiahao <jiahao.xue@nriat.com>
2023-05-05 10:19:17 +0800

    feat: 实现 Trie (前缀树)

---
+++ src/com/leetcode/trie/Solution208.java
@@ -0,0 +1,66 @@
+package com.leetcode.trie;
+
+/**
+ * 208. 实现 Trie (前缀树)
+ *
+ * @author cangtan
+ * @version V1.0
+ * @since 2023-04-28 17:59:00
+ */
+public class Solution208 {
+
+    private static class Trie {
+        private final Trie[] children;
+        private boolean isEnd;
+
+        public Trie() {
+            children = new Trie[26];
+            isEnd = false;
+        }
+
+        public void insert(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    node.children[index] = new Trie();
+                }
+                node = node.children[index];
+            }
+            node.isEnd = true;
+        }
+
+        public boolean search(String word) {
+            Trie trie = startsPrefix(word);
+            return trie != null && trie.isEnd;
+        }
+
+        public boolean startsWith(String prefix) {
+            return startsPrefix(prefix) != null;
+        }
+        
+        public Trie startsPrefix(String word) {
+            Trie node = this;
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                int index = ch - 'a';
+                if (node.children[index] == null) {
+                    return null;
+                }
+                node = node.children[index];
+            }
+            return node;
+        }
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));
+        System.out.println(trie.search("app"));
+        System.out.println(trie.startsWith("app"));
+        trie.insert("app");
+        System.out.println(trie.search("app"));
+    }
+}
