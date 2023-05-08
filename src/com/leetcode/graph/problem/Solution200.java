059bf577cbe7b5ca60ce58be648c046937adb5d0
xuejiahao <jiahao.xue@nriat.com>
2023-05-08 14:32:55 +0800

    feat: 岛屿数量-并查集

--- src/com/leetcode/graph/problem/Solution200.java
+++ src/com/leetcode/graph/problem/Solution200.java
@@ -23,7 +23,7 @@ public class Solution200 {
     }
 
     public void numIslandsHelper(char[][] grid, boolean[][] booleans, int i, int j) {
-        if (i< 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0' || booleans[i][j]) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0' || booleans[i][j]) {
             return;
         }
         booleans[i][j] = true;
@@ -31,6 +31,67 @@ public class Solution200 {
         numIslandsHelper(grid, booleans, i + 1, j);
         numIslandsHelper(grid, booleans, i, j - 1);
         numIslandsHelper(grid, booleans, i, j + 1);
+    }
+
+    /**
+     * 并查集
+     */
+    public int numIslandsUF(char[][] grid) {
+        int rows = grid.length;
+        int col = grid[0].length;
+        UnionFind uf = new UnionFind(grid);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    int idx = i * col + j;
+                    if (i + 1 < rows && grid[i + 1][j] == '1') {
+                        uf.union(idx, (i + 1) * col + j);
+                    }
+                    if (j + 1 < col && grid[i][j + 1] == '1') {
+                        uf.union(idx, idx + 1);
+                    }
+                }
+            }
+        }
+        return uf.count;
+    }
+
+    class UnionFind {
+        int[] parent;
+        int count;
+
+        public UnionFind(char[][] grid) {
+            int rows = grid.length;
+            int col = grid[0].length;
+            int n = rows * col;
+            parent = new int[n];
+            count = 0;
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < col; j++) {
+                    if (grid[i][j] == '1') {
+                        parent[i * col + j] = i * col + j;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        public int find(int i) {
+            if (parent[i] != i) {
+                parent[i] = find(parent[i]);
+            }
+            return parent[i];
+        }
+
+        public void union(int i, int j) {
+            int x = find(i);
+            int y = find(j);
+            if (x == y) {
+                return;
+            }
+            parent[x] = parent[y];
+            count--;
+        }
     }
 
     public static void main(String[] args) {
@@ -42,6 +103,6 @@ public class Solution200 {
                 {'0', '0', '0', '1', '1'},
         };
 //        System.out.println(grid.length);
-        System.out.println(solution.numIslands(grid));
+        System.out.println(solution.numIslandsUF(grid));
     }
 }
