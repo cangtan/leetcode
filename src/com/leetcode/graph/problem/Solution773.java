b1318f2f0e405d5b300ccdd183815681cd1d6d6b
xuejiahao <jiahao.xue@nriat.com>
2023-05-16 17:52:09 +0800

    feat: 滑动谜题（A* Search）

--- src/com/leetcode/graph/problem/Solution773.java
+++ src/com/leetcode/graph/problem/Solution773.java
@@ -2,6 +2,8 @@ package com.leetcode.graph.problem;
 
 import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Set;
 
@@ -67,11 +69,98 @@ public class Solution773 {
         return -1;
     }
 
+    /**
+     * 启发式搜索
+     */
+    public int slidingPuzzleAStar(int[][] board) {
+        StringBuilder startStr = new StringBuilder();
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                startStr.append(board[i][j]);
+            }
+        }
+        final String target = "123450";
+        if (target.equals(startStr.toString())) {
+            return 0;
+        }
+        PriorityQueue<AStar> queue = new PriorityQueue<>((a, b) -> a.f - b.f);
+        queue.add(new AStar(startStr.toString(), 0));
+        visited.add(startStr.toString());
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                AStar poll = queue.poll();
+                for (AStar aStar : listNext(poll)) {
+                    if (target.equals(aStar.status)) {
+                        return aStar.g;
+                    }
+                    if (!visited.contains(aStar.status)) {
+                        visited.add(aStar.status);
+                        queue.add(aStar);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+
+    private List<AStar> listNext(AStar aStar) {
+        List<AStar> result = new LinkedList<>();
+        int index = aStar.status.indexOf("0");
+        char[] chars = aStar.status.toCharArray();
+        for (int i : swapMap[index]) {
+            swap(chars, i, index);
+            result.add(new AStar(new String(chars), aStar.g + 1));
+            swap(chars, i, index);
+        }
+        return result;
+    }
+
+    private void swap(char[] chars, int i, int j) {
+        char temp = chars[i];
+        chars[i] = chars[j];
+        chars[j] = temp;
+    }
+
+    private static class AStar {
+        public static int[][] dist = {
+                {0, 1, 2, 1, 2, 3},
+                {1, 0, 1, 2, 1, 2},
+                {2, 1, 0, 3, 2, 1},
+                {1, 2, 3, 0, 1, 2},
+                {2, 1, 2, 1, 0, 1},
+                {3, 2, 1, 2, 1, 0}
+        };
+        private String status;
+        private int f;
+        private int g;
+        private int h;
+
+        public AStar(String status, int g) {
+            this.status = status;
+            this.g = g;
+            this.h = getH(status);
+            this.f = this.g + this.h;
+        }
+
+        public int getH(String status) {
+            int result = 0;
+            for (int i = 0; i < 6; i++) {
+                char ch = status.charAt(i);
+                if (ch != '0') {
+                    result += dist[i][ch - '1'];
+                }
+            }
+            return result;
+        }
+
+    }
+
     public static void main(String[] args) {
         Solution773 solution = new Solution773();
         int[][] board = {
                 {4, 1, 2}, {5, 0, 3}
         };
-        System.out.println(solution.slidingPuzzle(board));
+        System.out.println(solution.slidingPuzzleAStar(board));
     }
 }
