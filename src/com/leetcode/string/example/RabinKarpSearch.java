175ab1634c2210e5239aec200706ef4088c8ab9a
xuejiahao <jiahao.xue@nriat.com>
2023-06-09 11:02:45 +0800

    feat: RabinKarp算法注释

--- src/com/leetcode/string/example/RabinKarpSearch.java
+++ src/com/leetcode/string/example/RabinKarpSearch.java
@@ -2,7 +2,11 @@ package com.leetcode.string.example;
 
 /**
  * 拉宾卡普算法
- *
+ * 思想
+ * 1.假设子串的长度为M（pat）,目标字符串的长度为N（txt）
+ * 2.计算淄川的hash值hash_pat
+ * 3.计算目标字符串txt中每个长度为M的子串的hash值（共需要计算N-M+1次）
+ * 4.比价hash值：如果hash值不同，字符串必然不匹配；如果hash值相同，还需要使用朴素算法再次判断
  * @author cangtan
  * @version V1.0
  * @since 2023-06-09 10:05:00
